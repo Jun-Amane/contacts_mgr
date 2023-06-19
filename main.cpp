4fdf4972e26251edbe91660f9e7ea66b561c8ad1
JunASAKA <JunASAKA@zzy040330.moe>
2023-06-19 16:22:45 +0800

    fin

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main ()
 {
 
 #ifdef _WIN32
-  system ("CHCP 65001"); // For Windows.
+  system ("CHCP 65001"); // 仅在Win32下编译，设置控制台编码为UTF-8。
 #else
   setlocale (LC_ALL, "zh_CN.UTF-8"); // For POSIX.
 #endif
